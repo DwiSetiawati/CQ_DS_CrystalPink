ce6b31115c5de4babb309dd1a19c940f90057a08
DwiSetiawati <dwisetwa27@gmail.com>
2026-03-12 14:15:29 +0700

    Deklarasi variabel global SqlConnection conn; dan SqlCommand cmd; di dalam class

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -9,11 +9,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-
 namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
 
         public Form1()
         {
